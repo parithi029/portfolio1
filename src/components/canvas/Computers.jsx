5ddffe42e80ab5c8b3a7cb4b10c40c0044b37aa0
Parithi B <parithi029@gmail.com>
2025-09-30 01:07:37 +0530

    first commit

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -7,32 +7,17 @@ const Computers = ({isMobile}) => {
     const computer = useGLTF('./desktop_pc/scene.gltf');
 
   return (
-    <mesh >
+    <mesh>
+      {/* CONSOLIDATED HEMISPHERE LIGHT */}
       <hemisphereLight
-        intensity={1}
+        intensity={2.5} // Increased intensity to compensate for removing 3 other lights
         groundColor="black"
       />
-      <hemisphereLight
-        intensity={1}
-        groundColor="black"
-      />
-      <hemisphereLight
-        intensity={1}
-        groundColor="black"
-      />
-      <hemisphereLight
-        intensity={1}
-        groundColor="black"
-      />
+
+      {/* RETAINED POINT LIGHT */}
       <pointLight intensity={1} />
-      <spotLight
-        position={[-20, 50, 10]}
-        angle={0.20}
-        penumbra={1}
-        intensity={1}
-        castShadow
-        shadow-mapSize={1024}
-      />
+
+      {/* CONSOLIDATED SPOT LIGHT */}
       <spotLight
         position={[-20, 50, 10]}
         angle={0.20}
@@ -42,10 +27,11 @@ const Computers = ({isMobile}) => {
         shadow-mapSize={1024}
       />
       
+      {/* The rest of your code remains the same... */}
       <primitive
         object={computer.scene}
         scale={isMobile ? 0.55 : 0.75}
-        position={isMobile ? [0,-2, -0.9]:[0, -2.25, -1.5]}
+        position={isMobile ? [0, -2, -0.9] : [0, -2.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
